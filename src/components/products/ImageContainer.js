5f00a950d5143fcb8cb916336d47ccc8505af1e9
barat1378 <baratalihassanzada1378@gmail.com>
2023-02-16 12:11:20 +0330

    latest

--- src/components/products/ImageContainer.js
+++ src/components/products/ImageContainer.js
@@ -1,11 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Image from "next/image";
 import { Box } from "@mui/material";
 import style from "../../../styles/Product.module.css";
 const ImageContainer = ({ image }) => {
   return (
     <Box display={"flex"} p={2} justifyContent={"center"}>
-      <Image src={image} height="305px" alt="Not supported" fill />
+      <img className={style.image} src={image} alt={"Not Supported"} />
     </Box>
   );
 };
